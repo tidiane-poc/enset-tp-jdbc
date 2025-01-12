ebed10c2d036e6c32caa6d690609f166f158b2d7
Tidiane DIALLO <tidiane.diallo@teliman.ml>
2025-01-12 19:35:48 +0100

    First commit

---
+++ src/main/java/ma/enset/iibdcc/dao/JdbcConnection.java
@@ -0,0 +1,24 @@
+package ma.enset.iibdcc.dao;
+
+import java.sql.Connection;
+
+public class JdbcConnection {
+    private static Connection instance;
+    private static final String URL = "jdbc:mysql://localhost:3306/db_patients";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "NO_PASSWORD";
+
+    // static statement block is executed when the class is loaded into memory
+    static {
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver"); not needed since JDBC 4.0 automatically loads the driver
+            instance = java.sql.DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getInstance() {
+        return instance;
+    }
+}

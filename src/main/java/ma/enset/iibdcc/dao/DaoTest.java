6e3cd22c4257cd7f6a6210dd969570cd63bc3342
Tidiane DIALLO <tidiane.diallo@teliman.ml>
2025-01-12 19:55:21 +0100

    Adding findByEmail tests

--- src/main/java/ma/enset/iibdcc/dao/DaoTest.java
+++ src/main/java/ma/enset/iibdcc/dao/DaoTest.java
@@ -7,10 +7,14 @@ public class DaoTest {
 //        patientDao.save(patient);
         System.out.println("All patients:");
         patientDaoImpl.findAll().forEach(System.out::println);
+
         System.out.println("Patient with id 1:");
         var optionalPatient = patientDaoImpl.findOne(1L);
         System.out.println(optionalPatient.orElse(null));
-        assert(optionalPatient.isPresent());
+
+        System.out.println("Patient with email: amadou@gmail.com");
+        var optionalPatientByEmail = patientDaoImpl.findByEmail("amadou@gmail.com");
+        System.out.println(optionalPatientByEmail.orElse(null));
 //        patientDao.delete(3L);
     }
 }
